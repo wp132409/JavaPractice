8311c9eef746b7860c7844379897950971b6bcf3
wp132409 <imkimhyunwoo@naver.com>
2015-04-13 09:32:12 +0900

    dsajfjklasdfhkjads

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * 
+ * <pre>
+ * org.dimigo.oop
+ *    |_Car
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 4. 13.
+ * </pre>
+ *
+ * @author		: 김현우
+ * @version		: 1.0
+ */
+public class Car {
+
+	/**
+	 * @param args
+	 */
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public String getCompany() {
+		return company;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setCompany(String newCompany) {
+		company = newCompany;
+	}
+
+	public void setModel(String newModel) {
+		model = newModel;
+	}
+	
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+	
+	public void setMaxSpeed(int newMaxSpeed) {
+		maxSpeed = newMaxSpeed;
+	}
+	
+	public void setPrice(int newPrice) {
+		price = newPrice;
+	}
+
+}
