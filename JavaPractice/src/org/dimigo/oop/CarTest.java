c4260689c9d9463e3033d4d9e3a61dc4248e7985
wp132409 <imkimhyunwoo@naver.com>
2015-04-14 15:40:02 +0900

    asdfasdf

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -19,9 +19,13 @@ package org.dimigo.oop;
 public class CarTest {
 	
 	public static void main(String[] args) {
-		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
-		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
-		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+		Car3 car1 = new Car3("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
+		Car3 car3 = new Car3("삼성자동차", "SM7", "회색");
+		
+//		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+//		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+//		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
 		
 //		Car car1 = new Car();
 //		Car car2 = new Car();
