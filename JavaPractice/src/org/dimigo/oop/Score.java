d3761b29ec8109e50208c5a08a23b30a66d768e7
wp132409 <imkimhyunwoo@naver.com>
2015-05-12 15:44:37 +0900

    ㅁㄴㅇㄹㅁㄴㅇ

---
+++ JavaPractice/src/org/dimigo/oop/Score.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * <pre>
+ * org.dimigo.oop
+ *    |_Score
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 5. 12.
+ * </pre>
+ *
+ * @author		: 김현우
+ * @version		: 1.0
+ */
+public class Score {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("국어 점수 입력 => ");
+		int kScore = scanner.nextInt();
+		
+		System.out.print("수학 점수 입력 => ");
+		int mScore = scanner.nextInt();
+		
+		System.out.print("영어 점수 입력 => ");
+		int eScore = scanner.nextInt();
+		
+		//==================================
+		
+		System.out.println("<점수 출력>\n");
+		String ScoreSb = new StringBuilder("국어 점수 : ")
+								.append(kScore)
+								.append(" 점")
+								.append("\n")
+								.append("수학 점수 : ")
+								.append(mScore)
+								.append(" 점")
+								.append("\n")
+								.append("영어 점수 : ")
+								.append(eScore)
+								.append(" 점")
+								.append("\n")
+								.append("평균 : ")
+								.append((kScore + mScore + eScore)/3.0)
+								.append(" 점")
+								.append("\n")
+								.append("총점 : ")
+								.append(kScore + mScore + eScore)
+								.append(" 점").toString();
+		scanner.close();
+		
+		System.out.println(ScoreSb);
+		
+	}
+}
