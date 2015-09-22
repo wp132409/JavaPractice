b14cb0df315b4fa7865fc26fc3d1420db252c682
wp132409 <imkimhyunwoo@naver.com>
2015-09-22 14:46:24 +0900

    ㅁㄴㅇㅁㄴ

--- JavaPractice/src/org/dimigo/exception/AgeCheckException.java
+++ JavaPractice/src/org/dimigo/exception/AgeCheckException.java
@@ -21,7 +21,7 @@ public class AgeCheckException extends Exception{
 	public AgeCheckException() {}
 	
 	public AgeCheckException(Movie movie) {
-		super(movie.getTitle() + "은/는" + movie.getLimitAge() + " 이상 관람가입니다.");
+		super(movie.getTitle() + "은/는 " + movie.getLimitAge() + "세 이상 관람가입니다.");
 	}
 	
 }
