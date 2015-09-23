58ef81ee37d83ef539b5bc84da33659e9ec95754
wp132409 <imkimhyunwoo@naver.com>
2015-09-23 13:27:59 +0900

    asdf

---
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * <pre>
+ * org.dimigo.collection
+ *    |_MelonChart
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 9. 23.
+ * </pre>
+ *
+ * @author		: 김현우
+ * @version		: 1.0
+ */
+public class MelonChart {
+	
+	public static void main(String[] args) {
+		
+		List<Music> list = new ArrayList<Music>();
+		
+		list.add(new Music("바람이나 좀 쐐", "개리"));
+		list.add(new Music("보통연애", "박경"));
+		list.add(new Music("취향저격", "iKON"));
+		
+		System.out.println("--<< 멜론 챠트 >>--");
+		printList(list);
+		
+		list.add(1, new Music("레옹", "이유갓지"));
+		
+		System.out.println("--<< 2위 곡 추가 >>--");
+		printList(list);
+		
+		list.set(2, new Music("맙소사", "황태지"));
+		
+		System.out.println("--<< 3위 곡 변경 >>--");
+		printList(list);
+		
+		list.remove(3);
+		
+		System.out.println("--<< 4위 곡 삭제 >>--");
+		printList(list);
+		
+		System.out.println("--<< 전체 리스트 삭제 >>--");
+		list.clear();
+		printList(list);
+	}
+	
+	public static void printList(List<Music> list) {
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(i+1 + ". " + list.get(i).toString());
+		}
+		System.out.println();
+		
+	}
+	
+}
